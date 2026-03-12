a20e11c910714292ee07f5f7b281b5ba2391e554
AL-student-11 <255115666+AL-student-11@users.noreply.github.com>
2026-03-12 14:10:46 +0200

    Drop seconds from datetime string

--- src/js/guestbook.js
+++ src/js/guestbook.js
@@ -2,5 +2,5 @@ const dt = document.getElementById("time");
 const button = document.getElementById("button");
 
 button.addEventListener("click", function (event) {
-	dt.value = new Date().toUTCString();
+	dt.value = new Date().toUTCString().replace(/:\d{2} GMT$/, " GMT");
 });
